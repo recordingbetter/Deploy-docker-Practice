ea930f6cc25074974185d1719a3e7987cc3590df
Joe Donghee Cho <recordingbetter@gmail.com>
2017-07-05 15:12:51 +0900

    add admin

--- django_app/member/admin.py
+++ django_app/member/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import MyUser
+
+
+# 상속받은 User 모델에 img_profile을 추가했으므로, UserAdmin에도 img_profile을 추가하기 위해 fieldset을 재정의.
+# 이렇게 해야 admin 화면에 추가된 img_profile 필드가 나타난다.
+class MyUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'img_profile')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        )
+
+
+admin.site.register(MyUser, MyUserAdmin)
